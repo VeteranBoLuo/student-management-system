51f00f8e5ecefde365c9f898e9e80bea1c85f7f1
文俊邱 <7683883+wenjunqiu@user.noreply.gitee.com>
2021-08-30 16:40:27 +0800

    优化了一下代码

--- src/main/java/com/example/demo/utils/Check.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.utils;
-public class Check{
-    public static boolean isNotEmpty(Object obj) {
-        if(obj != null) {
-            return true;
-        }
-        return false;
-    }
-}
